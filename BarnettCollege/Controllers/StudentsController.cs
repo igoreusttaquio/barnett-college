0489c98cb1c83c10984a470f51dc05c1092cb823
Igor <igoreusttaquio@gmail.com>
2021-08-13 13:54:43 -0300

    Manages error reporting.

--- BarnettCollege/Controllers/StudentsController.cs
+++ BarnettCollege/Controllers/StudentsController.cs
@@ -153,9 +153,22 @@ namespace BarnettCollege.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var student = await _context.Students.FindAsync(id);
-            _context.Students.Remove(student);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            
+            if (student == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException /*ex*/)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
         }
 
         private bool StudentExists(int id)
