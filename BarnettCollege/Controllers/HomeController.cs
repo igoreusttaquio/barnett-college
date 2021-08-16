da05a51017e1130c874b97ede69ed866093f36a6
Igor <igoreusttaquio@gmail.com>
2021-08-16 12:07:48 -0300

    Created an about page and controller function with deal with.

--- BarnettCollege/Controllers/HomeController.cs
+++ BarnettCollege/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using BarnettCollege.Models;
+﻿using BarnettCollege.Data;
+using BarnettCollege.Models;
+using BarnettCollege.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,10 +15,12 @@ namespace BarnettCollege.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +37,20 @@ namespace BarnettCollege.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
